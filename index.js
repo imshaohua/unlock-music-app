7ac6b8df0206a9901362f9b0c0857e5fffa47d6b
shaohua.hsh <shaohua.hsh@alipay.com>
2020-11-18 17:17:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 960, height: 720})
+
+  mainWindow.loadFile('index.html')
+
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
---
+++ precache-manifest.5df80a6e40c6a7e602f34fa21008d7e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fb398ca6e929151231",
+    "url": "css/app.58add593.css"
+  },
+  {
+    "revision": "bd211beedbe431d39ea8",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "f824776f119a06617a079be128899416",
+    "url": "daf933914a50180f6d92.worker.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6d307bcd72ed70bc3905d7f85cc01e96",
+    "url": "index.html"
+  },
+  {
+    "revision": "62fb398ca6e929151231",
+    "url": "js/app.80cebe87.js"
+  },
+  {
+    "revision": "bd211beedbe431d39ea8",
+    "url": "js/chunk-vendors.83c5f48e.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  }
+]);
